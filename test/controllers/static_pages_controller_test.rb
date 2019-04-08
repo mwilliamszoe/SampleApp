85a88cc9e0924c002a956ef08b2faae8d1f95ba5
github username <mwilliamszoe@gmail.com>
2019-04-08 17:08:26 -0500

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,20 +7,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     end
 
     test "should get home" do
-        get static_pages_home_url
+        get root_path
         assert_response :success
         assert_select "title", "Ruby on Rails Tutorial Sample App"
     end
 
-    test "should get home" do
-        get static_pages_help_url
+    test "should get helpt" do
+        get help_path
         assert_response :success
         assert_select "title", "Help | #@{base_title}"
     end
 
-    test "should get home" do
-        get static_pages_about_url
+    test "should get about" do
+        get about_path
         assert_response :success
         assert_select "title", "About | #@{base_title}"
     end
+
+    test "should get contact" do
+        get contact_path
+        assert_response :success
+        assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
 end
